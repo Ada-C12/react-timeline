8337e50bc49220d18f3e5885f76e9277583b85a0
Diana <dianah46@gmail.com>
2019-11-20 14:48:38 -0800

    setup timeline and timelineEvent to pass in props and read from json data

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  // need to display, Timestamp, events.person, events.status, events.timestamp
+  return (
+    <div>
+      <h1>This is the TimelineEvent</h1>
+    
+      <p> {props.person}</p>
+      <p> {props.status}</p>
+      <p> {props.timeStamp}</p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
