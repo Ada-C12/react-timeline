1283cdfd3d442e5070bd15b8d5531513547e2149
m.dove@live.com <m.dove@live.com>
2019-11-20 15:33:43 -0800

    Formatted and listing all timeline events

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import Timeline from './Timeline';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  let timestamp = <Timestamp time={props.timestamp} />
+  return(
+    <section className = "timeline-event">
+      <p className = "event-person">{props.person}</p>    
+      <p className = "event-status">{props.status} </p>
+      <p className = "event-time">{timestamp}</p>
+
+    </section>
+  )
 }
 
 export default TimelineEvent;
