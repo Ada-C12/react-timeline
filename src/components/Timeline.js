4af1249cdd62e2b950217a0835eaf43aa6c8a7cd
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-20 15:36:22 -0800

    more comments

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,6 +7,7 @@ const Timeline = (props) => {
   // events is an array of single event hashes
   let events = props.events;
 
+  // map thru events, make a TimelineEvent component obj for each one
   let timelines = events.map ((event, i) => {
     return <TimelineEvent person={event.person} status={event.status} timeStamp={event.timeStamp}/>;
   });
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,9 +3,13 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
+  // props is passed in via line 12 in Timeline.js, use it to make new new TimelineEvent Component obj
+  // timestamp is sent off to Timestamp Component for processing
   const timestamp = <Timestamp time={props.timeStamp} />;
   const person = props.person;
   const status = props.status;
+
+  // Take passed in data and make bulleted list JSX.  this is going to Timeline.js, then App.js
   return(
     <ul>
       <li>TIME: {timestamp}</li>
