4065d03780199ea0dd676fb68fc65af57ef488df
Janice Huang <janicehuang@gmail.com>
2019-11-20 15:02:49 -0800

    start with TimelineEvent

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <p>Person: {props.person} </p>
+    <p>Status: {props.status} </p>
+    <p>Timestamp: {Timestamp </p>
+  );
 }
 
 export default TimelineEvent;
