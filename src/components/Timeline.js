5e96219cfcac7857660e3aff40f96143c97137ee
Kristina <kristina.tanya@gmail.com>
2019-11-20 15:46:41 -0800

    wave 2 complete

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,22 +5,12 @@ import TimelineEvent from './TimelineEvent';
 
 // will take a list of timeline events as a prop &
 // render a list of TimelineEvent components
-const Timeline = (prop) => {
-  const events = [
-    {
-      person: "Ada Lovelace",
-      status: "ada@lovelace.uk",
-    },
-    {
-      person: "Katherine Johnson",
-      status: "kat@nasa.gov",
-    },
-  ];
-  
-  const timelineEventComponents = events.map((event, i) => {
+const Timeline = (props) => {
+
+  const timelineEventComponents = props.events.map((event, i) => {
     return (
       <ul key={i}>
-        <li><TimelineEvent person={ event.person } status={ event.status } /></li>
+        <li><TimelineEvent person={ event.person } status={ event.status } timestamp={ event.timeStamp } /></li>
       </ul>
     );
   });
