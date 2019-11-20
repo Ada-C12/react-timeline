d729c87f65df0c77854276e90fa55e26d5289f98
Ga-Young Jin <gyjinn@gmail.com>
2019-11-20 15:32:52 -0800

    completed waves 1 and 2

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,37 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  // const allEvents = {
+  //   for (let i = 0; i < props.length; i += 1) {
+  //     return(
+  //       <section key={i}>
+  //       <li>
+  //         <TimelineEvent person={ props[i].person } status={props[i].status } timestamp={ props[i].timeStamp } />
+  //       </li>
+  //     </section>
+  //     );
+  //   };
+  // }
+
+  const allEvents = props.events.map((event, i) => {
+    return (
+      // each child in a list must have a unique key
+      <section key={i}>
+        <li>
+          <TimelineEvent person={ event.person } status={event.status } timestamp={ event.timeStamp } />
+        </li>
+      </section>
+    );
+  });
+
+  return (
+    <section>
+      <ul>
+          {allEvents}
+      </ul>
+    </section>
+  )
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return(
+    <div>
+      person: { props.person }
+      status: { props.status}
+      timestamp: <Timestamp time={ props.timestamp } />
+    </div>
+  );
 }
 
 export default TimelineEvent;
