3264e24d9ed10da8913457869ad1f15e12709d67
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-11-20 15:39:43 -0800

    finished timeline, timelineEvent, and App

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+import timeline from '../data/timeline'
 
 const Timeline = () => {
-  
-  return;
+  const timelineComponents = timeline["events"].map((data, i) => {
+    return(
+      <TimelineEvent 
+        person={data.person}
+        status={data.status}
+        timestamp={data.timeStamp}
+        />
+
+    )
+  })
+  return(
+    <section className="timeline"> 
+
+    <p> {timelineComponents} </p>
+    
+
+    
+    
+    </section>
+  );
 }
 
 export default Timeline;
