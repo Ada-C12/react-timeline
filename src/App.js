718d89189d89daf465b3a0cc27a22a3ab76fc636
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-20 15:45:23 -0800

    implemented TimelineEvent component function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
+import TimelineEvent from './components/TimelineEvent';
+
 function App() {
   console.log(timelineData);
+
+  const event = {
+      "person": "Aubrey Tang",
+      "status": "The art of computer programming is a blend of mathematics and poetry.",
+      "timeStamp": "2018-05-18T22:41:19Z"
+  }
 
   // Customize the code below
   return (
@@ -14,6 +22,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+      <TimelineEvent person={event.person} status={event.status} timeStamp={event.timeStamp}/>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
-}
+const TimelineEvent = (props) => {
+
+  return(
+    <section>
+      <p>Person = {props.person}</p>
+      <p>Status =  {props.status}</p>
+      <p>TimeStamp = <Timestamp timeStamp={props.timeStamp}/></p>
+    </section>
+  );
+};
 
 export default TimelineEvent;
