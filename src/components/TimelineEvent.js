3e7b2cd56f72783ea3795047baf05c4c5e3cb9af
Linnea Johnson <linnea.johnson1@gmail.com>
2019-11-20 15:38:57 -0800

    Added thumbs up icon

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import thumbsUp from './thumb-up.svg';
 
-
+function callAlert(e) {
+  e.preventDefault();
+  alert("this worked!!!!!");
+}
 
 const TimelineEvent = (props) => {
   return (
@@ -10,6 +14,9 @@ const TimelineEvent = (props) => {
       <p className="event-person"> Person: {props.person} </p>
       <p className="event-status"> Status: {props.status} </p>
       <p className="event-time"><Timestamp time = {props.timestamp}></Timestamp></p>
+      <p className="event-like-button">
+        <img onClick={callAlert} className="thumbs-up-icon" src={thumbsUp} alt="thumbs up"/>
+      </p>
     </div>
   );
 }
