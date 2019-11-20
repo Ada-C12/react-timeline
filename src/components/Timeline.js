6a203a92e6d51922b761bb2b0e0dced537ad4d0f
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-20 15:40:14 -0800

    Added const events in Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (prop) => {
+  const events = prop.events.map((event, i) => {
+    return (
+      <TimelineEvent key={i}
+      person={ event.person } 
+      status={ event.status } 
+      timestamp={ event.timestamp } />
+    );
+  });
+  return (
+    <ul className="student-collection">
+      {events}
+    </ul>
+  );
+};
 
 export default Timeline;
