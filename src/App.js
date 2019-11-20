2ff322efbdaa0e25051e4431913d8ef395382f4d
Monick <monick.keo@gmail.com>
2019-11-20 14:34:10 -0800

    was able to hardcode and show props on App.js. working on Timestamp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -14,6 +15,11 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent
+          person="momo"
+          status="cool"
+          timestamp="2019-11-09"
+        />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+
+  return (
+    <section>
+      <h3>ONE EVENT</h3>
+      <p>This event on <Timestamp/>: {props.person} -- {props.status}</p>
+    </section>
+  )
 }
 
 export default TimelineEvent;
