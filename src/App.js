19f209fd65c114a83fd0138cf953cd3197881430
Alice <alicesunhi@gmail.com>
2019-11-20 14:06:42 -0800

    timelineevent works with hardcoded stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import timelineData from './data/timeline.json';
-import Timeline from './components/Timeline';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import timelineData from "./data/timeline.json";
+import Timeline from "./components/Timeline";
+import TimelineEvent from "./components/TimelineEvent";
+import Timestamp from "./components/Timestamp";
 
 function App() {
   console.log(timelineData);
-
   // Customize the code below
   return (
     <div className="App">
+      <TimelineEvent person="Ada" status="good" />
+
       <header className="App-header">
         <h1 className="App-title">Application title</h1>
       </header>
-      <main className="App-main">
-      </main>
+      <main className="App-main"></main>
     </div>
   );
 }
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './TimelineEvent.css';
-import Timestamp from './Timestamp';
+import React from "react";
+import "./TimelineEvent.css";
+import Timestamp from "./Timestamp";
 
-const TimelineEvent = () => {
-  
-  return;
-}
+const TimelineEvent = props => {
+  let currentTime = Timestamp(Date.now());
+
+  return (
+    <div>
+      <p>{props.person}</p>
+      <p>{props.status}</p>
+      <p>{currentTime}</p>
+    </div>
+  );
+};
 
 export default TimelineEvent;
