09646b7b96c66aef63d07b597864233b840346ee
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2019-11-20 15:32:06 -0800

    completed project

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+const events = props.events;
+
+const eventsComp = events.map((event,i) => {
+  return(
+    <p key = {i}>
+    <TimelineEvent
+      person={event.person}
+      timestamp={event.timestamp}
+      status={event.status}
+    />
+  </p>
+  )
+});
+
+
+return (
+  <section className = "timeline">
+    <p>{eventsComp}</p>
+  </section>
+    )
+};
+
 
 export default Timeline;
