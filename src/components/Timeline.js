136faa88ee0e1e6426cbc54ef45401781281d459
Paige Davenport <davenport.paige@outlook.com>
2019-11-20 14:38:10 -0800

    Forgot to commit! Completed TimelineEvent and Timeline components.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const events = props.events.map((event, i) => {
+    return (
+    <div key={i}>
+      <TimelineEvent 
+        person={event.person}
+        status={event.status}
+        timestamp={event.timeStamp}
+      />
+    </div>
+    );
+  }); 
+
+  return(
+    <section className="timeline">
+      {events}
+    </section>
+  );
 }
 
 export default Timeline;
