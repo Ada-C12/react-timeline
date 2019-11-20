59680f9637cd6f2bd258c8eec108f04ec62f9ab9
Brianna Kemp <kemp.bri@gmail.com>
2019-11-20 15:32:15 -0800

    Both waves implemented

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const events = props.events;
+
+  const timelineComponents = events.map((event, i) => {
+      return (
+        <p key={i}>
+          <TimelineEvent person={ event.person } status={ event.status } timestamp={ event.timestamp }/>
+        </p>
+      );
+    });
+
+  return (
+      <section className="timeline">
+        <p>
+          {timelineComponents}
+        </p>
+      </section>
+    );
 }
 
 export default Timeline;
