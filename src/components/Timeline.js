8896016a373e9c074b2039b6a4f989560ba4244d
Mira <calopter@gmail.com>
2019-11-20 14:54:16 -0800

    timeline event component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = ({ events }) => {
+  console.log(events);
+  return(
+    <section className='timeline'>
+      <TimelineEvent event={ events[0] } />
+    </section>
+  );
 }
 
 export default Timeline;
