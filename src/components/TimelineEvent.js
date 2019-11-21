af0d410950c8f1ddb760fa96245a3bb9f7433210
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-11-20 20:31:34 -0800

    css aded

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,10 +4,10 @@ import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
   return(
-    <section>
-      <p> name = {props.person} </p>
-      <p>status = {props.status} </p> 
-      <p>time = <Timestamp time = {props.time} /> </p>
+    <section className="timeline-event">
+      <p className="event-person"> {props.person} </p>
+      <p className="event-status"> {props.status} </p> 
+      <p className="event-time">  <Timestamp time = {props.time} /> </p>
   </section>
   );
 };
