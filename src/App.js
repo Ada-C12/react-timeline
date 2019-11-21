5a7238887669bc78afd64f58c80b65e858595c29
Kristina <kristina.tanya@gmail.com>
2019-11-20 16:03:18 -0800

    some css styling applied

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
-        <Timeline events={timelineData.events}/>
+        <div><Timeline events={timelineData.events}/></div>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -13,10 +13,10 @@ const TimelineEvent = (props) => {
   
   
   return (
-  <div>
-    <p>{props.person}</p>
-    <p>{props.status}</p>
-    <p><Timestamp time={props.timestamp} /></p>
+  <div className='timeline-event'>
+    <p className='event-person'>{props.person}</p>
+    <p className='event-status'>{props.status}</p>
+    <p className='event-time'><Timestamp time={props.timestamp} /></p>
   </div>
   )
 };
