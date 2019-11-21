202111b9d8b3648d8df97c214c16793091e26455
brilatimer <brihiramine@gmail.com>
2019-11-20 17:22:01 -0800

    setup and wave 1 complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import Timestamp from './components/Timestamp';
+import TimelineEvent from './components/TimelineEvent';
+
 
 function App() {
   console.log(timelineData);
@@ -11,9 +14,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent event={timelineData.events[0]}/>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <div>
+    <h3>{props.event.person}</h3>
+    <h3>{props.event.status}</h3>
+    <h3>{props.event.timeStamp}</h3>
+    <h3>TimeStamp time={props.event.timeStamp}</h3>
+
+  </div>
+
+  );
 }
 
 export default TimelineEvent;
