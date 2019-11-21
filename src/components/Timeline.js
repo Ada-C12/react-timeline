f5f6085a40ef646ef1ef4b5435f8f6b58d7e990f
Janice Huang <janicehuang@gmail.com>
2019-11-20 20:26:07 -0800

    finally get things connected and working

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = ({events}) =>
+  events.map((event, i) => {
+    return (
+      <li key={i}>
+        <TimelineEvent {...event} />
+      </li>
+    )
+  })
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = (props) => {
-  return (
-    <p>Person: {props.person} </p>
-    <p>Status: {props.status} </p>
-    <p>Timestamp: {Timestamp </p>
-  );
-}
+const TimelineEvent = ({person, status, timeStamp}) =>
+    <div>
+      <p>Person: {person} </p>
+      <p>Status: {status} </p>
+      <Timestamp time={timeStamp}/>
+    </div>
+
 
 export default TimelineEvent;
