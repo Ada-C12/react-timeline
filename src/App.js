ef53aed42e3828b8a495175caa87f5b898251692
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-20 21:46:41 -0800

    completed wave 1

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
+  const time = new Date()
 
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Application title!</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="Mariya" timestamp={time} status="Hello World!!!"/>
       </main>
     </div>
   );
