193eae628b6a1552ce8352aaf807ec3f5799b57f
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-20 16:24:26 -0800

    Changed dates in JSON so I could see how timestamp component works and hardcoded props in app.js. I think this is wave one done?

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent'
+
 
 function App() {
-  console.log(timelineData);
+  console.log(timelineData)
 
-  // Customize the code below
+  const events = timelineData.events
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s Social Media Feed</h1>
       </header>
       <main className="App-main">
+      <TimelineEvent person='Adele Goldberg' status='In Smalltalk, everything happens somewhere else.' timeStamp='2019-11-20T08:12:03Z'  />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import Timeline from './Timeline';
+import timelineData from '../data/timeline.json';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <div className="timeline-event"> 
+      <div className="event-person"> {props.person}</div>
+      <div className="event-status">{props.status}</div>
+      <div className="event-time"><Timestamp time={props.timeStamp}/></div>
+    </div>
+  );
 }
 
 export default TimelineEvent;
