ab7fde22aa635d764deb47894128f83e9dbe4bef
KKennedyCodes <holmstedtk@gmail.com>
2019-11-20 16:13:47 -0800

    Project Completed

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  console.log(props);
+  return(
+    <div className = "timeline-event">
+      <p className = "event-person">{props.user.person}</p>
+      <p className = "event-status">{props.user.status}</p>
+      <p className = "event-time"><Timestamp time = {props.user.timeStamp} /></p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
