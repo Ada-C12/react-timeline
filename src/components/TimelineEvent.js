8238e56f96b2948303edc2416e8b89fef81dbfe1
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-21 22:28:20 -0800

    Trying to get closure with the closure function

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,13 +4,18 @@ import Timestamp from './Timestamp';
 import thumbsUp from './thumb_up-24px.svg'
 
 const TimelineEvent = (props) => {
-
   let clickCount = 0;
+
   function callAlert(e) {
     e.preventDefault();
-    clickCount += 1;
-    alert(`You like it ${clickCount} times!`);
+    const buildIncrementer = () => {
+      clickCount += 1;
+      document.getElementById('click-count-span').textContent = clickCount;
+      return clickCount;  
+    }
+    return buildIncrementer();
   }
+  // let incrementer = callAlert;
   return (
     <div className="timeline-event">
       <p className="event-person">
@@ -24,6 +29,11 @@ const TimelineEvent = (props) => {
       </p>
       <p className="event-like-button">
         <img onClick={callAlert} className="thumb" src={thumbsUp} alt="thumbs up" />
+        <span>
+          You liked this <span id="click-count-span">
+          
+          </span> times
+        </span>
       </p>
     </div>
   );
