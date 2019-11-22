474c6d549888550c76766e2852f32fc7fe8933aa
Erika <erika.maust@gmail.com>
2019-11-21 22:52:18 -0800

    Added a lot...also fixed the main tag issue in App causing error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -14,8 +14,8 @@ function App() {
         <h1 className="App-title">Ada's Current Social Media Feed:</h1>
       </header>
       <main className="App-main">
+      <Timeline events={timelineData.events}/>
       </main>
-      <Timeline />
     </div>
   );
 }
